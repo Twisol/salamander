ad044e521e3bf4582fdc99900ad4ce93a715bf6a
Twisol <twisolar@gmail.com>
2011-01-13 02:30:41 -0800

    Use antialiased lines

--- lib/salamander/drawing/line.rb
+++ lib/salamander/drawing/line.rb
@@ -3,14 +3,14 @@ module Salamander::Drawing
     def draw_to (x2, y2)
       x1, y1 = position
       move_to(x2, y2)
-      SDL::Gfx.lineColor(surface, x1.round, y1.round, x2.round, y2.round, color)
+      SDL::Gfx.aalineColor(surface, x1.round, y1.round, x2.round, y2.round, color)
     end
     
     def draw (distance)
       x1, y1 = position
       move distance
       x2, y2 = position
-      SDL::Gfx.lineColor(surface, x1.round, y1.round, x2.round, y2.round, color)
+      SDL::Gfx.aalineColor(surface, x1.round, y1.round, x2.round, y2.round, color)
     end
   end
 end
